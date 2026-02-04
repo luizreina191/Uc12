2e45088655122b0de754d36bd61a6f0644d6a425
luizreina191 <luizreina2002@gmail.com>
2026-02-04 14:53:16 -0300

    feat: update project configuration and add Prettier support

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import { app, BrowserWindow } from 'electron';
+
 function createWindow() {
- const win = new BrowserWindow({
- width: 800,
- height: 600
- });
- win.loadFile('./index.html');
+	const win = new BrowserWindow({
+		width: 800,
+		height: 600
+	});
+	win.loadFile('./src/index.html');
 }
+
 app.whenReady().then(createWindow);
